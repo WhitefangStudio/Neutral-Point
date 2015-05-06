d67d2a022351108d625a3806a649ac7a5674d3ae
Alex Van de Sande <alexvandesande@hotmail.ca>
2015-05-06 11:33:39 -0700

    Worked on main menu

---
+++ Assets/MyScripts/MainMenu/MapSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapSelect : MonoBehaviour {
+	public Transform planet1;
+	public PlanetsRotate PR;
+	bool b=false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void goToP1(){
+		b = true;
+		PR.radius = 5;
+		PR.Sun=planet1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (b == true) {
+			transform.position=Vector3.Lerp (transform.position,planet1.position,Time.deltaTime);
+			if(Vector3.Distance(transform.position,planet1.position)<15){
+				b=false;
+				transform.parent=planet1;
+				PR.setup();
+			}
+		}
+	}
+}
---
+++ Assets/MyScripts/MainMenu/PlanetsRotate.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetsRotate : MonoBehaviour {
+	public float radius;
+	public Transform Sun;
+	Vector3 rotate;
+	public float speed;
+
+	public bool isMoon;
+	// Use this for initialization
+
+	void OnEnable(){
+		setup ();
+	}
+	public void setup(){
+		float scale = Random.Range (5, 15);
+		//transform.localScale = new Vector3 (scale, scale, scale);
+		float randX = Random.Range (-4*radius,4*radius);
+		if (randX < 0) {
+			randX-=1*radius;
+		}
+		if (randX >= 0) {
+			randX+=1*radius;
+		}
+		float randY= Random.Range (-4*radius,4*radius);;
+
+		if (randY < 0) {
+			randY-=1*radius;
+		}
+		if (randY >= 0) {
+			randY+=1*radius;
+		}
+
+		transform.position = new Vector3 (Sun.transform.position.x+randX, Sun.transform.position.y+Random.Range (-5*radius/10,5*radius/10),
+		                                  Sun.transform.position.z+randY);
+
+
+		if(!isMoon)
+			speed = Mathf.Log (Vector3.Distance (Sun.position, transform.position));
+		if (isMoon)
+			speed = Mathf.Log (Vector3.Distance (Sun.position, transform.position))*6;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Sun != null) {
+			//Debug.Log (Vector3.Distance(Sun.position,transform.position));
+			//Debug.Log (Vector3.Distance (transform.position,Sun.position));
+			transform.RotateAround (Sun.position,Vector3.up,  speed * Time.deltaTime);
+
+		}
+	}
+}
